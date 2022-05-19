3d0a95f4c0c1dc507568f41ed7f55204b3cf9494
prayugo <pray.arsian24@gmai.com>
2022-05-19 21:48:56 +0700

    first commit

---
+++ controller.ts
@@ -0,0 +1,9 @@
+
+class controller{
+    constructor(){
+        console.log('Hello World');
+    }
+    saveRepository(data: string): Promise<string | null> {
+        return "Hello" as any
+    }
+}
